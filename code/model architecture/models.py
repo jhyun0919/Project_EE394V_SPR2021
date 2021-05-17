66d37882ea5db7d4c3d638d785538a52946879d6
Park, JeeHyun <jhyun19@gmail.com>
2021-05-17 14:50:42 -0500

    updated
    model architecture codes

--- code/model architecture/models.py
+++ code/model architecture/models.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
 from dgl.nn import GraphConv, GATConv
+import dgl.function as fn
 
 ##################################################
 # Dense Net
@@ -13,20 +13,24 @@ from dgl.nn import GraphConv, GATConv
 class DenseNet(nn.Module):
     def __init__(self, input_dim, output_dim, node_num):
         super(DenseNet, self).__init__()
+        self.input_dim = input_dim
         self.node_num = node_num
         self.output_dim = output_dim
         self.flatten = nn.Flatten()
-        self.dense_1 = nn.Linear(node_num, 512)
-        self.dense_2 = nn.Linear(512, 512)
-        self.dense_3 = nn.Linear(512, self.node_num * self.output_dim)
-        self.tanh = nn.Tanh()  # tanh
+        self.dense_1 = nn.Linear(input_dim * node_num, 128)
+        self.dense_2 = nn.Linear(128, 128)
+        self.dense_3 = nn.Linear(128, self.node_num * self.output_dim)
+        # self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
         x = self.flatten(x)
+        x = torch.reshape(x, (-1, 1, self.input_dim * self.node_num))
         x = F.relu(self.dense_1(x))
         x = F.relu(self.dense_2(x))
         x = self.dense_3(x)
-        x = torch.reshape(x, (-1, self.node_num, self.output_dim))
+        x = torch.reshape(x, (self.node_num, self.output_dim))
+        # x = self.sigmoid(x)
         x = self.tanh(x)
         return x
 
@@ -70,28 +74,34 @@ class ConvNet(nn.Module):
 ##################################################
 
 # Ref:  https://docs.dgl.ai/en/latest/api/python/nn.pytorch.html#graphconv
+# Ref: https://github.com/dmlc/dgl/tree/master/examples/pytorch/gcn
 
 
-class GraphConvNet(nn.Module):
-    def __init__(self, in_feats, out_feats):
-        super(GraphConvNet, self).__init__()
-        self.gcn_1 = GraphConv(in_feats, 100)
-        self.gcn_2 = GraphConv(100, 200)
-        self.gcn_3 = GraphConv(200, out_feats)
-        self.elu = nn.ELU()
-        self.sigmoid = nn.Sigmoid()  # tanh
+class GCN(nn.Module):
+    def __init__(self, g, in_feats, n_hidden, n_classes, n_layers, activation, dropout):
+        super(GCN, self).__init__()
+        self.g = g
+        self.layers = nn.ModuleList()
+        # input layer
+        self.layers.append(GraphConv(in_feats, n_hidden, activation=activation))
+        # hidden layers
+        for _ in range(n_layers - 1):
+            self.layers.append(GraphConv(n_hidden, n_hidden, activation=activation))
+        # output layer
+        self.layers.append(GraphConv(n_hidden, n_classes))
+        self.dropout = nn.Dropout(p=dropout)
+        self.tanh = nn.Tanh()
+        # self.sigmoid = nn.Sigmoid()
 
-    def forward(self, g, x):
-        x = self.gcn_1(g, x)
-        x = self.elu(x)
-        print(x.size())
-        x = self.gcn_2(g, x)
-        x = self.elu(x)
-        print(x.size())
-        x = self.gcn_3(g, x)
-        x = self.sigmoid(x)
-        print(x.size())
-        return x
+    def forward(self, features):
+        h = features
+        for i, layer in enumerate(self.layers):
+            if i != 0:
+                h = self.dropout(h)
+            h = layer(self.g, h)
+        h = self.tanh(h)
+        # h = self.sigmoid(h)
+        return h
 
 
 ##################################################
@@ -101,25 +111,79 @@ class GraphConvNet(nn.Module):
 # Ref:
 # https://docs.dgl.ai/en/latest/api/python/nn.pytorch.html#gatconv
 # https://docs.dgl.ai/en/latest/tutorials/models/1_gnn/9_gat.html?highlight=gat
+# https://github.com/dmlc/dgl/blob/master/examples/pytorch/gat/gat.py
 
 
-class GraphAttentionNet(nn.Module):
-    def __init__(self, in_feats, out_feats, num_heads):
-        super(GraphAttentionNet, self).__init__()
-        self.gat_1 = GATConv(in_feats, 100, num_heads)
-        self.gat_2 = GATConv(100 * num_heads, 200, num_heads)
-        self.gat_3 = GATConv(200 * num_heads, out_feats, 1)
-        self.elu = nn.ELU()
-        self.sigmoid = nn.Sigmoid()  # tanh
+class GAT(nn.Module):
+    def __init__(
+        self,
+        g,
+        num_layers,
+        in_dim,
+        num_hidden,
+        num_classes,
+        heads,
+        activation,
+        feat_drop,
+        attn_drop,
+        negative_slope,
+        residual,
+    ):
+        super(GAT, self).__init__()
+        self.g = g
+        self.num_layers = num_layers
+        self.gat_layers = nn.ModuleList()
+        self.activation = activation
+        self.tanh = nn.Tanh()
+        # self.sigmoid = nn.Sigmoid()
+        # input projection (no residual)
+        self.gat_layers.append(
+            GATConv(
+                in_dim,
+                num_hidden,
+                heads[0],
+                feat_drop,
+                attn_drop,
+                negative_slope,
+                False,
+                self.activation,
+            )
+        )
+        # hidden layers
+        for l in range(1, num_layers):
+            # due to multi-head, the in_dim = num_hidden * num_heads
+            self.gat_layers.append(
+                GATConv(
+                    num_hidden * heads[l - 1],
+                    num_hidden,
+                    heads[l],
+                    feat_drop,
+                    attn_drop,
+                    negative_slope,
+                    residual,
+                    self.activation,
+                )
+            )
+        # output projection
+        self.gat_layers.append(
+            GATConv(
+                num_hidden * heads[-2],
+                num_classes,
+                heads[-1],
+                feat_drop,
+                attn_drop,
+                negative_slope,
+                residual,
+                None,
+            )
+        )
 
-    def forward(self, g, x):
-        x = self.gat_1(g, x)
-        x = self.elu(x)
-        print(x.size())
-        x = self.gat_2(g, x)
-        x = self.elu(x)
-        print(x.size())
-        x = self.gat_3(g, x)
-        x = self.sigmoid(x)
-        print(x.size())
-        return x
+    def forward(self, inputs):
+        h = inputs
+        for l in range(self.num_layers):
+            h = self.gat_layers[l](self.g, h).flatten(1)
+        # output projection
+        h = self.gat_layers[-1](self.g, h).mean(1)
+        # h = self.sigmoid(h)
+        h = self.tanh(h)
+        return h
